e9999d097d3627eef7f28d4bc301fb6f848aa471
Nati Mal <natanaelmalita@gmail.com>
2021-07-14 22:51:59 +0300

    Add custom search function

--- js/bundle.js
+++ js/bundle.js
@@ -4,8 +4,33 @@ const baguetteBox = require('baguettebox.js');
 
 baguetteBox.run('.gallery');
 
-const search = new Filter('search', 'data-caption');
 
+
+ document.getElementById("search").addEventListener("keyup", mySearch);
+
+
+  function mySearch() {
+    // Declare variables
+    var input, filter, gallery, imgs, a, i;
+    input = document.getElementById('search');
+    filter = input.value.toUpperCase();
+    gallery = document.getElementById("gallery");
+    imgs = gallery.getElementsByTagName('a');
+  
+    // Loop through all gallery items, and hide those who don't match the search query
+    for (i = 0; i < imgs.length; i++) {
+      a = imgs[i].getAttribute("data-caption");
+      
+      if (a.toUpperCase().indexOf(filter) > -1) {
+        imgs[i].style.display = "";
+      } else {
+        imgs[i].style.display = "none";
+      }
+    }
+  } 
+  
+
+  
 },{"baguettebox.js":2}],2:[function(require,module,exports){
 /*!
  * baguetteBox.js
